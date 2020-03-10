9a6ddb61e1ac09cb80b24ce6edfa749da80d8cca
Gabriel A. Hayes <gabriel.speed.bullock@gmail.com>
2020-03-10 10:53:23 -0500

    Set CWD.

--- Run.hx
+++ Run.hx
@@ -1,11 +1,12 @@
 class Run {
     public static function main() {
+        var args = Sys.args();
         try {
-            var args = Sys.args();
-            args = args.slice(0, args.length-1);
+            var lastArg = args.splice(args.length-1, 1);
+            Sys.setCwd(lastArg[0]);
             hxmake.HxMake.main(args);
         } catch(e:Dynamic) {
-            trace('Bad args: ${Sys.args()}');
+            trace('Bad args: ${args}');
             trace(e);
         }
     }
